ce9436e845d06d961329a36cb45987caedb4ca98
npg2108 <npg2108@cumc.columbia.edu>
2017-10-18 17:46:30 -0400

    website v2 with knitrBootstrap

---
+++ docs/build_site.R
@@ -0,0 +1,2 @@
+rmarkdown::render('index.Rmd', knitrBootstrap::bootstrap_document("CUMC Data Science Homepage", theme="Spacelab", highlight="Sunburst",theme.chooser=T,highlight.chooser = T, menu=T))
+

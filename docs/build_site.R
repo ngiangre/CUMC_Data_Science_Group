f209d83a4766755af75c1a3348585d3244b3a1de
npg2108 <npg2108@cumc.columbia.edu>
2017-11-07 18:23:06 -0500

    Verion of website-cytof clickable well

--- docs/build_site.R
+++
@@ -1,7 +0,0 @@
-#rmarkdown::render('index.Rmd', 
-#                  knitrBootstrap::bootstrap_document(
-#                    "CUMC Data Science Homepage", 
-#                    theme="Spacelab", highlight="Sunburst",
-#                    theme.chooser=T,highlight.chooser = T, menu=T)
-#                  )
-#rmarkdown::render('index.Rmd')
